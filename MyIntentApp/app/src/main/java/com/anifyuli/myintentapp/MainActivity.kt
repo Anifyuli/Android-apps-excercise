ce79f96fb362b31eafdd334d4a4e0f171e280220
Anifyuli <anifyuliansyah@gmail.com>
2021-12-09 18:00:57 +0700

    Finishing intent exercise

--- MyIntentApp/app/src/main/java/com/anifyuli/myintentapp/MainActivity.kt
+++ MyIntentApp/app/src/main/java/com/anifyuli/myintentapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.anifyuli.myintentapp
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +15,35 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
         btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveWithDataActivity : Button = findViewById(R.id.btn_move_data_activity)
+        btnMoveWithDataActivity.setOnClickListener(this)
+
+        val btnDialNumber : Button = findViewById(R.id.btn_dial_number)
+        btnDialNumber.setOnClickListener(this)
     }
+
+
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_move_activity -> {
                 val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
                 startActivity(moveIntent)
+            }
+
+            R.id.btn_move_data_activity -> {
+                val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy, Anif")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, "18")
+                startActivity(moveWithDataIntent)
+            }
+
+            R.id.btn_dial_number -> {
+                val phoneNumber = "088806025022"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
+
             }
         }
     }
