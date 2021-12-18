df2b4039f16571ebd5b27273b5b5db1adad29b4b
Anifyuli <anifyuliansyah@gmail.com>
2021-12-18 16:17:59 +0700

    Still error and cann't resolve for few times

---
+++ MyRecyclerView/app/src/main/java/com/anifyuli/myrecyclerview/HeroesData.kt
@@ -0,0 +1,4 @@
+package com.anifyuli.myrecyclerview
+
+class HeroesData {
+}
--- MyViewAndViews/app/src/main/java/com/anifyuli/myviewandviews/MainActivity.kt
+++ MyViewAndViews/app/src/main/java/com/anifyuli/myviewandviews/MainActivity.kt
@@ -7,7 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        supportActionBar?.title = "Google Pixel"
+        if (supportActionBar != null) {
+            supportActionBar!!.title = "Google Pixel"
+        }
     }
 }
